22bd12cd3ac09ff025ad957e61f80d6dea64195f
Badar Wildanie <badar.serpent@gmail.com>
2020-09-25 14:01:44 +0700

    Reset tanpa template, krn mau templating lagi

--- application/views/data/buku/daftar.php
+++
@@ -1,9 +0,0 @@
-<?php $this->view('template/header.php')?>
-<?php $this->view('template/navigation.php')?>
-
-<!-- KONTEN UTAMA DISINI -->
-
-<?php $this->view('template/js_script.php') ?>
-<script type="text/javascript">
-</script>
-<?php $this->view('template/footer.php')?>
